c38150602f455dab60f8c993a2e19c4e466adbb2
Francisco J. Vasquez <fvasquez@argentuminc.com>
2018-11-28 10:01:31 -0400

    Project Import

---
+++ src/com/sonarqube/course/SonarQube.java
@@ -0,0 +1,15 @@
+package com.sonarqube.course;
+
+import java.util.logging.Logger;
+
+public class SonarQube {
+	
+	private static final Logger LOGGER = Logger.getLogger(SonarQube.class.getName());
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo");
+
+	}
+
+}
